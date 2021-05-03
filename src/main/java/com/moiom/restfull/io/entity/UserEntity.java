a90ec547cb79420fb25843cf228fa1eaa5abfcb2
Pawel <zamorskipawel@wp.pl>
2021-05-03 21:40:50 +0100

    Add UserEntity class.

--- src/main/java/com/moiom/restfull/io/entity/UserEntity.java
+++ src/main/java/com/moiom/restfull/io/entity/UserEntity.java
@@ -1,80 +1,110 @@
-package com.moiom.restfull.shared.dto;
+package com.moiom.restfull.io.entity;
 
 import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * 
  * @author MoioM
- * DTO - Data Transfer Object. 
  * 
+ * A JPA entity class is a POJO (Plain Old Java Object) class, i.e. an ordinary Java class 
+ * that is marked (annotated) as having the ability to represent objects in the database.
  */
-public class UserDto implements Serializable{
-	private static final long serialVersionUID = 6744406845133371696L;
+
+@Entity(name="users")
+public class UserEntity implements Serializable {
+	private static final long serialVersionUID = -9177036627836208911L;
+	
+	@Id
+	@GeneratedValue
 	private long id;
+	
+	@Column(nullable = false)
 	private String userId; // public User Id (not the auto-incremented)
+	
+	@Column(nullable = false, length = 50)
 	private String firstName;
-	private String lastname;
+	
+	@Column(nullable = false, length = 50)
+	private String lastName;
+	
+	@Column(nullable = false, length = 120)
 	private String email;
-	private String password;
+	
+	@Column(nullable = false)
 	private String encryptedPassword; // password to be stored in DB 9It must be encrypted
+
 	private String emailVerificationToken;
+	
+	@Column(nullable = false)
 	private Boolean emailVerificationStatus = false;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	public String getLastname() {
-		return lastname;
+
+	public String getLastName() {
+		return lastName;
 	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	public String getEncryptedPassword() {
 		return encryptedPassword;
 	}
+
 	public void setEncryptedPassword(String encryptedPassword) {
 		this.encryptedPassword = encryptedPassword;
 	}
+
 	public String getEmailVerificationToken() {
 		return emailVerificationToken;
 	}
+
 	public void setEmailVerificationToken(String emailVerificationToken) {
 		this.emailVerificationToken = emailVerificationToken;
 	}
+
 	public Boolean getEmailVerificationStatus() {
 		return emailVerificationStatus;
 	}
+
 	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
 		this.emailVerificationStatus = emailVerificationStatus;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 	
 }
