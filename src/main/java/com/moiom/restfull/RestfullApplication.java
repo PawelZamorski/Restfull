5a515c23f0974c30130a40473a3fb34a59206ced
Pawel <zamorskipawel@wp.pl>
2021-05-05 22:21:03 +0100

    Add BCryptPasswordEncoder to encrypt password before storing in DB

--- src/main/java/com/moiom/restfull/RestfullApplication.java
+++ src/main/java/com/moiom/restfull/RestfullApplication.java
@@ -2,12 +2,19 @@ package com.moiom.restfull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class RestfullApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/moiom/restfull/service/impl/UserServiceImpl.java
+++ src/main/java/com/moiom/restfull/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.moiom.restfull.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.moiom.restfull.UserRepository;
@@ -16,6 +17,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Override
 	public UserDto createUser(UserDto user) {
 		// check if User exist
@@ -24,7 +28,7 @@ public class UserServiceImpl implements UserService {
 		// create entity to be stored in DB
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
-		userEntity.setEncryptedPassword("testEncryptedPassword");
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		userEntity.setUserId(Utils.generateUserId(20));
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
